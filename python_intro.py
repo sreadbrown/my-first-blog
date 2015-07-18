45db14d8236858f65ac590caac6fb5e7a5b2f642
Sarah Read-Brown <sreadbrown@gmail.com>
2015-07-18 12:00:56 -0700

    pre lunch save

---
+++ python_intro.py
@@ -0,0 +1,7 @@
+def hi(name):
+	print('Hi ' + name + '!')
+
+girls =['Rachel', 'Sarah', 'Jenny', 'Chloe']
+for name in girls:
+	hi(name)
+	print('Next Girl')
---
+++ srbvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Sarah_RB/djangogirls/srbvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
